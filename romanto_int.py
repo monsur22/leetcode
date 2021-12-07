21a9588a6db8f907c8e8e12f7caf7d233131b560
monsur22 <monsurahmedshafiq@gmail.com>
2021-12-07 23:36:18 +0600

    leet code

--- romanto_int.py
+++ romanto_int.py
@@ -1,1 +1,15 @@
 dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+total = 0
+curr = 0
+prev = 0
+
+for i in range(len(input())):
+	curr = dic[input()[i]]
+	if curr >= prev:
+		total += curr
+	else:
+		total -= curr
+	prev = curr
+
+return total
